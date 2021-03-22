fb133a87e06efd9c7acc27785a0bb6e969fe9808
Viktor Gamov <viktor@confluent.io>
2021-03-22 15:35:49 -0400

    Adding consumer / producer factories

---
+++ feature/src/main/kotlin/io/confluent/developer/ktor/Consumer.kt
@@ -0,0 +1,30 @@
+package io.confluent.developer.ktor
+
+import com.typesafe.config.Config
+import io.confluent.developer.extension.configMap
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.ProducerConfig
+import java.util.*
+
+fun <K, V> buildConsumer(config: Config): KafkaConsumer<K, V> {
+    val bootstrapServers = config.getList("ktor.kafka.bootstrap.servers")
+
+    // common config
+    val commonConfig = configMap(config, "ktor.kafka.properties")
+
+    // get consumer config
+    val consumerConfig = configMap(config, "ktor.kafka.consumer")
+
+    val consumerProperties: Properties = Properties().apply {
+        putAll(commonConfig)
+        putAll(consumerConfig)
+        put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers.unwrapped())
+    }
+    return KafkaConsumer(consumerProperties)
+}
+
+fun <K, V> createKafkaConsumer(config: Config, topic: String): KafkaConsumer<K, V> {
+    val consumer = buildConsumer<K, V>(config)
+    consumer.subscribe(listOf(topic))
+    return consumer
+}
---
+++ feature/src/main/kotlin/io/confluent/developer/ktor/Producer.kt
@@ -0,0 +1,22 @@
+package io.confluent.developer.ktor
+
+import com.typesafe.config.Config
+import io.confluent.developer.extension.configMap
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig.*
+import java.util.*
+
+fun <K, V> buildProducer(config: Config): KafkaProducer<K, V> {
+    val bootstrapServers = config.getList("ktor.kafka.bootstrap.servers")
+    // common config
+    val commonConfig = configMap(config, "ktor.kafka.properties")
+    // get producer config
+    val producerConfig = configMap(config, "ktor.kafka.producer")
+    // creating properties
+    val producerProperties: Properties = Properties().apply {
+        putAll(producerConfig)
+        putAll(commonConfig)
+        put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers.unwrapped())
+    }
+    return KafkaProducer(producerProperties)
+}
