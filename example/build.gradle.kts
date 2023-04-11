929770493180138d818bf8fa32b7bbf380160388
Viktor Gamov <gamussa@gmail.com>
2023-04-11 20:50:48 +0200

    added insomnia example

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -69,10 +69,10 @@ dependencies {
 
 
     //junit5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
-    testImplementation("org.assertj:assertj-core:3.22.0")
+    testImplementation("org.assertj:assertj-core:3.24.2")
 
     testImplementation("org.apache.kafka:kafka-streams-test-utils:$ak_version")
 }
